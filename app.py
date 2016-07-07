8c52a4c7906a35076f306ff16436848b67dae0f1
Visgean Skeloru <me@visgean.me>
2016-07-07 02:02:08 +0200

    expand transcations

--- app.py
+++ app.py
@@ -6,19 +6,40 @@ eth_client = Web3(IPCProvider())
 app = Flask(__name__)
 
 
+SHORT_TRANSACTION_IGNORE_FIELDS = ['blockHash', 'blockNumber']
+
+def get_short_transaction(transaction):
+    """
+    :param transaction: transcation hash
+    :return: Returns dictionary of trans without block info
+    """
+    info = eth_client.eth.getTranscation(transaction)
+    for field in SHORT_TRANSACTION_IGNORE_FIELDS:
+        del info[field]
+    return info
+
+
+
 @app.route('/')
 @app.route('/<int:block>')
-def show_block(block=None):
+def block(block=None):
     if block is None:
         block = eth_client.eth.getBlockNumber()
-    details = eth_client.eth.getBlock(block)
+    block_info = eth_client.eth.getBlock(block)
 
     # hide annoying bloom filter:
-    details['logsBloom'] = details['logsBloom'][:20] + '...'
+    block_info['logsBloom'] = block_info['logsBloom'][:20] + '..McBLOOMBLOOM'
+
+    # unpack the transactions
+    block_info['transactions'] = [
+        get_short_transaction(t) for t in block_info['transactions']
+    ]
+
+
 
     return render_template(
         'block.html',
-        details=details,
+        block_info=block_info,
         block=block,
     )
 
