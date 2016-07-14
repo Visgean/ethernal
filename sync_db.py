69c3f4247ccd1f31ec6c74b608f1c43b55c6acdd
Visgean Skeloru <me@visgean.me>
2016-07-14 02:00:05 +0200

    indexes for tables
    this type of script sucks, I will have to structure the db somehow.

--- sync_db.py
+++ sync_db.py
@@ -7,8 +7,22 @@ try:
     r.db_create('ethernal').run(conn)
     r.db('ethernal').table_create('blocks').run(conn)
     r.db('ethernal').table_create('transactions').run(conn)
+    conn.close()
 except r.errors.ReqlOpFailedError:
     pass
+
+try:
+    conn = r.connect("localhost", 28015, 'ethernal')
+    r.table('blocks').index_create('hash').run(conn)
+    r.table('blocks').index_create('number').run(conn)
+
+    r.table('transactions').index_create('hash').run(conn)
+    r.table('transactions').index_create('number').run(conn)
+    r.table('transactions').index_create('from').run(conn)
+    r.table('transactions').index_create('to').run(conn)
+except KeyboardInterrupt:
+    pass
+
 
 if __name__ == '__main__':
     chain = BlockChain()
