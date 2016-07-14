d24cae8792ad6ac9d9548419de16126e8fcced4c
Visgean Skeloru <me@visgean.me>
2016-07-14 02:47:50 +0200

    get connection refactor

--- cached_tools.py
+++ cached_tools.py
@@ -5,65 +5,72 @@ from functools import wraps
 
 
 CACHE_SPAN = 60 * 60 * 2
-conn = r.connect("localhost", 28015, 'ethernal')
-try:
-    r.table_create('cache').run(conn)
-except r.errors.ReqlOpFailedError:
-    pass
+
+
+def get_conn():
+    return r.connect("localhost", 28015, 'ethernal')
 
 
 def cache(f, live=CACHE_SPAN):
     @wraps(f)
     def wrapper(*args, **kwargs):
-        previous = list(r.table('cache').filter({
-            'func_name': f.__name__,
-            'args': args,
-            'kwargs': kwargs,
-        }).filter(r.row['time'] > (time.time() - live)).run(conn))
+        with get_conn() as conn:
+            previous = list(r.table('cache').filter({
+                'func_name': f.__name__,
+                'args': args,
+                'kwargs': kwargs,
+            }).filter(r.row['time'] > (time.time() - live)).run(conn))
 
-        if previous:
-            return previous[0]['result']
+            if previous:
+                return previous[0]['result']
 
-        result = f(*args, **kwargs)
-        r.table('cache').insert({
-            'func_name': f.__name__,
-            'args': args,
-            'kwargs': kwargs,
-            'result': result,
-            'time': time.time()
-        }).run(conn)
-        return result
+            result = f(*args, **kwargs)
+            r.table('cache').insert({
+                'func_name': f.__name__,
+                'args': args,
+                'kwargs': kwargs,
+                'result': result,
+                'time': time.time()
+            }).run(conn)
+            return result
     return wrapper
 
 
 @cache
 def mined_blocks(address):
-    return r.table('blocks').filter({'miner': address}).count().run(conn)
+    with get_conn() as conn:
+        return r.table('blocks').filter({'miner': address}).count().run(conn)
 
 
 def transactions_filter(account, t_type='from'):
-    return r.table('blocks').concat_map(
-        lambda b: b['transactions']
-    ).filter(
-        lambda t: t[t_type] == account
-    )
+    return r.table('transactions').filter({t_type: account}, index=t_type)
 
 
 @cache
 def transactions_sent(account, x, y):
-    return transactions_filter(account, 'from')[x:y].run(conn)
+    with get_conn() as conn:
+        return transactions_filter(account, 'from')[x:y].run(conn)
 
 
 @cache
 def transactions_received(account, x, y):
-    return transactions_filter(account, 'to')[x:y].run(conn)
+    with get_conn() as conn:
+        return transactions_filter(account, 'to')[x:y].run(conn)
 
 
 @cache
 def transactions_sent_count(account):
-    return transactions_filter(account, 'from').count().run(conn)
+    with get_conn() as conn:
+        return transactions_filter(account, 'from').count().run(conn)
 
 
 @cache
 def transactions_received_count(account):
-    return transactions_filter(account, 'to').count().run(conn)
+    with get_conn() as conn:
+        return transactions_filter(account, 'to').count().run(conn)
+
+
+@cache
+def transaction_count():
+    with get_conn() as conn:
+        return r.table('transactions').count().run(conn)
